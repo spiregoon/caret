f59b615389afc3fbf92de2b9abee99c97085853f
spiregoon <asdkill123@163.com>
2017-02-03 21:54:22 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var shopApp = angular.module('shopApp', ['ngRoute']);
+
+// configure our routes
+shopApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/success', {
+            templateUrl : 'partial/success.html',
+            controller  : 'loginController'
+        })
+
+});
